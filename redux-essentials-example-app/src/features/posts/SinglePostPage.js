2af52ec5120e810c7a008c3ec71c44b6be5db0c7
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-07-27 20:12:28 +0900

    Show reaction buttons on SinglePostPage

--- redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -28,6 +29,8 @@ export const SinglePostPage = ({ match }) => {
           <TimeAgo timestamp={post.date} />
         </div>
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
+
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
