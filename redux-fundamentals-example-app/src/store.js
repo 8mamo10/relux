dd4d43f594c54f4483d537c88fc468fe3574f6e5
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-08-22 21:08:35 +0900

    Try using middleware

--- redux-fundamentals-example-app/src/store.js
+++ redux-fundamentals-example-app/src/store.js
@@ -1,6 +1,7 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
 import { sayHiOnDispatch, includeMeaningOfLife } from './exampleAddons/enhancers'
+import { print1, print2, print3 } from './exampleAddons/middleware'
 
 let preloadedState
 const persistedTodosString = localStorage.getItem('todos')
@@ -12,7 +13,11 @@ if (persistedTodosString) {
 }
 
 //const store = createStore(rootReducer, preloadedState)
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
-const store = createStore(rootReducer, undefined, composedEnhancer)
+
+//const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+//const store = createStore(rootReducer, undefined, composedEnhancer)
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const store = createStore(rootReducer, middlewareEnhancer)
 
 export default store
