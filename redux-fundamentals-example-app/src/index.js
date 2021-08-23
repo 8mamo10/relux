9c6bd57bb120ee375e08e4e088e2c1b16fe10f82
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-08-24 02:00:03 +0900

    Pass the store with Provider

--- redux-fundamentals-example-app/src/index.js
+++ redux-fundamentals-example-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
 
@@ -39,7 +41,9 @@ store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
