7f3e9371c4c7c79c954addc5efa96fbbb6d3fac1
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-08-24 02:14:04 +0900

    Bugfix

--- redux-fundamentals-example-app/src/features/todos/TodoList.js
+++ redux-fundamentals-example-app/src/features/todos/TodoList.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const selectTodos = state => state.TodoListItem
+const selectTodos = state => state.todos
 
 const TodoList = () => {
   const todos = useSelector(selectTodos)
 
-  const renderedListItems = totos.map(todo => {
+  const renderedListItems = todos.map(todo => {
     return <TodoListItem key={todo.id} todo={todo} />
   })
 
-  return <ul className="todo-list">renderedListItems</ul>
+  return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
--- redux-fundamentals-example-app/src/features/todos/TodoListItem.js
+++ redux-fundamentals-example-app/src/features/todos/TodoListItem.js
@@ -1,36 +1,18 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 
 import { ReactComponent as TimesSolid } from './times-solid.svg'
 
 import { availableColors, capitalize } from '../filters/colors'
 
-const selectTodoById = (state, todoId) => {
-  return state.todos.find((todo) => todo.id === todoId)
-}
-
-// Destructure `props.id`, since we just need the ID value
-const TodoListItem = ({ id }) => {
-  // Call our `selectTodoById` with the state _and_ the ID value
-  const todo = useSelector((state) => selectTodoById(state, id))
+const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
   const { text, completed, color } = todo
 
-  const dispatch = useDispatch()
-
-  const handleCompletedChanged = () => {
-    dispatch({ type: 'todos/todoToggled', payload: todo.id })
+  const handleCompletedChanged = (e) => {
+    onCompletedChange(e.target.checked)
   }
 
   const handleColorChanged = (e) => {
-    const color = e.target.value
-    dispatch({
-      type: 'todos/colorSelected',
-      payload: { todoId: todo.id, color },
-    })
-  }
-
-  const onDelete = () => {
-    dispatch({ type: 'todos/todoDeleted', payload: todo.id })
+    onColorChange(e.target.value)
   }
 
   const colorOptions = availableColors.map((c) => (
